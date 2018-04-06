6396dd9a5fda808874b3d608d3dca663170d0e50
Majd Maghamez <majdmaghamez@gmail.com>
2018-04-06 03:52:13 -0400

    I found that i can replace this page with a redirect

--- main/controllers/notFound.php
+++ main/controllers/notFound.php
@@ -1,40 +1,16 @@
 <?php namespace main\controllers;
 
-    use main\layouts\bootstrap\main;
     class notFound extends Controller
     {
         protected function onGet()
         {
-            $layoutTemplate = new main ( );
-            $html   = "<!DOCTYPE html>\n";
-            $html   .= "<html lang=\"en\">\n";
-            $html   .= "\t<head>\n";
-            $html   .= $layoutTemplate->render_header ( [ "TITLE" => "404 Page Not Found"], ".notFound { margin-top: 15%; } .header { font-size:200px; }" );
-            $html   .= "\t</head>\n";
-            $html   .= "\t<body>\n";
-            $html   .= "\t\t<div class=\"container\">\n";
-            $html   .= "\t\t\t<div class=\"row justify-content-center align-items-center\">\n";
-            $html   .= "\t\t\t\t<div class=\"col-md-6 col-lg-6 col-xl-6 notFound center\">\n";
-            $html   .= "\t\t\t\t\t<h1 class=\"header\">404</h1>\n";
-            $html   .= "\t\t\t\t\t<h1>Page Not Found <i class=\"fas fa-exclamation\"></i></h1>\n";
-            $html   .= "\t\t\t\t\t<p>The page you have requested cannot be found.</p>\n";
-            $html   .= "\t\t\t\t</div>\n";
-            $html   .= "\t\t\t</div>\n";
-            $html   .= "\t\t\t<div class=\"row justify-content-center align-items-center\">\n";
-            $html   .= "\t\t\t\t<div class=\"col-md-4 col-lg-4 col-xl-4 center\">\n";
-            $html   .= "\t\t\t\t\t<a class=\"btn btn-light btn-outline-light\" href=\"" . $GLOBALS ["RELATIVE_TO_ROOT"] . "/Login\">Back to Login</a>\n";
-            $html   .= "\t\t\t\t</div>\n";
-            $html   .= "\t\t\t</div>\n";
-            $html   .= "\t\t</div>\n";
-            $html   .= $layoutTemplate->render_footer ( array ( ) );
-            $html   .= "\t</body>\n";
-            $html   .= "</html>\n";
-
-            echo $html;
+            http_response_code (404 );
+            redirect( '/404.html' );
         }
 
         protected function onPost()
         {
-            $this->onGet();
+            http_response_code( 404 );
+            redirect( '/404.html' );
         }
     }
