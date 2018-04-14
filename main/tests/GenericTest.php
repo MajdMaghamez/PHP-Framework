32f75d1fd3eca6a35a6ffd33bfddc0be55f2f98f
Majd Maghamez <majdmaghamez@gmail.com>
2018-04-13 21:04:53 -0400

    for testing components

---
+++ main/tests/GenericTest.php
@@ -0,0 +1,17 @@
+<?php namespace main\tests;
+
+    use main\controllers\Controller;
+
+    class GenericTest extends Controller
+    {
+
+        protected function onGet()
+        {
+            // TODO: Implement onGet() method.
+        }
+
+        protected function onPost()
+        {
+            // TODO: Implement onPost() method.
+        }
+    }
